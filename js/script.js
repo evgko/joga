7d961992b4e5493bc52c24223a8cdc1992667ca6
jackson <jackson3290@mail.ru>
2019-03-25 07:48:47 +0300

    Lesson 13

--- js/script.js
+++ js/script.js
@@ -115,4 +115,46 @@ tabContent[i].addEventListener('click', function() {
 });
 }
 
+//Form
+
+let message = {
+    loading: 'Загрузка...',
+    succes: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так...'
+};
+
+let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault;
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open = ('POST', '../server.php');
+        request.setRequestHeader ('Content-Type', 'application/x-www-form-urlencoded');
+    
+        let formData = new FormData(form);
+        request.send(formData);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4){
+                statusMessage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.succes;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+
+    });
+
 });
